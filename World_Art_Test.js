2e41dd1ceb0c4ccd11d7a60cc972541cb422060d
OrTem76 <tensorrab6@gmail.com>
2022-12-26 16:10:26 +0300

    patch

--- World_Art_Test.js
+++ World_Art_Test.js
@@ -1,10 +1,12 @@
 // @ts-check
+
+// npx playwright test  - Запуск теста, npx playwright show-report - репорт,
+// npg playwright test --headed  - открыть в браузере.
 const { test, expect } = require('@playwright/test');
 
-    test('First test', async (page)=>(
+    test('First test', async ({page})=> {
         await page.goto('http://www.world-art.ru/')
         await page.locator('main_page').click()
-        )
-    );
+    });
 
 
